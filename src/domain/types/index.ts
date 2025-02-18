de19c0e504ef75d3e9485d08e430ad320f2bc28d
Orlando Pinto <orlandopinto.freelance@gmail.com>
2025-02-18 21:33:53 +0100

    corrección archivo de barrido

--- src/domain/types/index.ts
+++ src/domain/types/index.ts
@@ -1,10 +1,6 @@
-export * from './permissions.type'
-export * from './actions.type'
 export * from './api-result-response.type'
 export * from './auth.type'
+export * from './enviroment-data.type'
 export * from './resourses.type'
-export * from './roles.type'
 export * from './sign-token.type'
 export * from './system-user.type'
-export * from './enviroment-data.type'
-
